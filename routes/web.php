1334e2adfec075d7729083f4831e5162961fcf45
Mecene <mecenephrygien@gmail.com>
2021-11-29 13:00:25 +0100

    ajout de classe IsAdmin dans Kernel

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'v1','middleware'=>['auth','is_admin']], function(){
+    
+});
