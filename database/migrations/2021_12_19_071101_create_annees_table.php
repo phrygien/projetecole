376493fc31c6a4d673a2eeaa97ed0c9529474aae
Phrygien <84671612+phrygien@users.noreply.github.com>
2021-12-19 08:15:44 +0100

    Implementation G-annee scolaire

---
+++ database/migrations/2021_12_19_071101_create_annees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnneesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('annees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('debut');
+            $table->string('fin');
+            $table->string('etat')->default(0);
+            $table->unsignedBigInteger('ateblissement_id');
+            $table->foreign('etablissement_id')->references('id')->on('etablissements');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('annees');
+    }
+}
